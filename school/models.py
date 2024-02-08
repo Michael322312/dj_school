07804be79dffee32378eef5b10ab4471d57b5499
Michael322312 <zomisha0808@gmail.com>
2024-02-08 15:26:48 +0200

    Edited models

--- school/models.py
+++ school/models.py
@@ -9,7 +9,7 @@ class Class(models.Model):
     grade = models.IntegerField()
     date = models.DateField()
     time = models.TimeField()
-    subject = models.ManyToManyField(Subject)
+    subject = models.ManyToManyField(Subject, related_name="classes")
     
 
 class Teacher(models.Model):
