19e992122f1c38cc1d41e0097c587d87785fb721
Michael322312 <zomisha0808@gmail.com>
2024-02-08 14:55:51 +0200

    Created models for school

--- school/models.py
+++ school/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length = 63)
+    teacher = models.ForeignKey("Teacher", on_delete = models.DO_NOTHING)
+
+class Class(models.Model):
+    grade = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+    subject = models.ManyToManyField(Subject)
+    
+
+class Teacher(models.Model):
+    name = models.CharField(max_length = 63)
+    age = models.IntegerField()
+
+
+class Student(models.Model):
+    name = models.CharField(max_length = 63)
+    age = models.IntegerField()
+    grade = models.IntegerField()
+    
