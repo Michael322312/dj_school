8258bd3535c5abc85505f720cfefa5c371edb493
Michael322312 <zomisha0808@gmail.com>
2024-02-08 17:15:00 +0200

    Edited models and created create funcs and for them interface

--- school/models.py
+++ school/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 class Subject(models.Model):
     name = models.CharField(max_length = 63)
-    teacher = models.ManyToManyField("Teacher")
+    teacher = models.ManyToManyField("Teacher", related_name="subjects")
 
 class Class(models.Model):
     grade = models.IntegerField()
     date = models.DateField()
     time = models.TimeField()
-    subject = models.ManyToManyField(Subject, related_name="classes")
-    
+    subject = models.ForeignKey(Subject, on_delete = models.DO_NOTHING, default=None)
+    teacher = models.ForeignKey("Teacher", on_delete = models.DO_NOTHING, default=None)
 
 class Teacher(models.Model):
     name = models.CharField(max_length = 63)
